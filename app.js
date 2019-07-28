72baab2f5c2dfee77d911d92bff80d7845f6644d
khmelovsky <a.i.khmelovsky@gmail.com>
2019-07-28 19:24:16 +0300

    Add new todos & validation

---
+++ app.js
@@ -0,0 +1,26 @@
+const addRecord = document.querySelector('.add');
+const todoList = document.querySelector('.todos');
+
+const generatetemplate = (todo) => {
+
+    const html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>`;
+
+    todoList.innerHTML += html;  
+};
+
+addRecord.addEventListener('submit', event => {
+
+    event.preventDefault();
+    const todo = addRecord.add.value.trim();
+ 
+    if(todo.length) {
+        generatetemplate(todo);
+        addRecord.reset();
+    }
+    //console.log(todo);
+
+});
